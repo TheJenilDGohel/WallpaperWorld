3d754d56ffd0c1d367989f9061af7360b0ba0bc2
Jenil D Gohel <j.d.gohel4200@gmail.com>
2022-07-17 22:26:10 +0530

    Save Wallpaper Done

--- lib/screens/detailedimage_screen.dart
+++ lib/screens/detailedimage_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:wallpaperworld/theme/theme.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
 
 class DetailedImageScreen extends StatefulWidget {
   final String imgUrl;
@@ -14,6 +18,57 @@ class DetailedImageScreen extends StatefulWidget {
 }
 
 class _DetailedImageScreenState extends State<DetailedImageScreen> {
+  String _platformVersion = 'Unknown';
+  String __heightWidth = "Unknown";
+
+// Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initAppState() async {
+    String platformVersion;
+    String _heightWidth;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    try {
+      platformVersion =
+          await WallpaperManager.platformVersion ?? 'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    try {
+      int height = await WallpaperManager.getDesiredMinimumHeight();
+      int width = await WallpaperManager.getDesiredMinimumWidth();
+      _heightWidth =
+          "Width = " + width.toString() + " Height = " + height.toString();
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+      _heightWidth = "Failed to get Height and Width";
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      __heightWidth = _heightWidth;
+      _platformVersion = platformVersion;
+    });
+  }
+
+  Future<void> setWallpaper() async {
+    try {
+      String url = widget.imgUrl;
+      int location = WallpaperManager
+          .HOME_SCREEN; // or location = WallpaperManager.LOCK_SCREEN;
+      var file = await DefaultCacheManager().getSingleFile(url);
+      final bool result =
+          await WallpaperManager.setWallpaperFromFile(file.path, location);
+      if (result) {
+        wallpaperUpdated(context);
+      }
+    } on PlatformException {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -36,7 +91,7 @@ class _DetailedImageScreenState extends State<DetailedImageScreen> {
                 Container(
                   height: MediaQuery.of(context).size.height,
                   width: MediaQuery.of(context).size.width,
-                  color: Color(0xff1c1b1b).withOpacity(0.6),
+                  color: Color(0xff1c1b1b).withOpacity(0.2),
                 ),
                 Container(
                   height: MediaQuery.of(context).size.height,
@@ -55,33 +110,52 @@ class _DetailedImageScreenState extends State<DetailedImageScreen> {
                             colors: [MyTheme.baseBlack, MyTheme.darkGrey],
                           ),
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            "Set This Wallppaer"
-                                .text
-                                .semiBold
-                                .size(16)
-                                .white
-                                .make(),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            "Image Will Be Saved In Gallary"
-                                .text
-                                .size(14)
-                                .ellipsis
-                                .red300
-                                .make(),
-                          ],
+                        child: InkWell(
+                          onTap: () {
+                            setWallpaper();
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              "Set This Wallppaer"
+                                  .text
+                                  .semiBold
+                                  .size(16)
+                                  .white
+                                  .make(),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              "Image Will Be Saved In Gallary"
+                                  .text
+                                  .size(14)
+                                  .ellipsis
+                                  .red300
+                                  .make(),
+                            ],
+                          ),
                         ),
                       ),
                       SizedBox(
                         height: 16,
                       ),
-                      "Cancel".text.white.bold.make(),
+                      InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child: Container(
+                            width: MediaQuery.of(context).size.width / 3.9,
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: MyTheme.orange, width: 2),
+                              borderRadius: BorderRadius.circular(30),
+                              gradient: LinearGradient(
+                                colors: [MyTheme.baseBlack, MyTheme.darkGrey],
+                              ),
+                            ),
+                            child: "Cancel".text.white.center.bold.make()),
+                      ),
                       SizedBox(
-                        height: 50,
+                        height: 20,
                       )
                     ],
                   ),
@@ -96,4 +170,13 @@ class _DetailedImageScreenState extends State<DetailedImageScreen> {
       ),
     );
   }
+}
+
+wallpaperUpdated(context) {
+  Navigator.pop(context);
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text("Wallpaper Updated !"),
+    ),
+  );
 }
